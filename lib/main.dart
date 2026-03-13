6ac3998cb345157348136d1cb82b286e02a2daa7
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-03-13 19:55:25 +0200

    feat(cross): Implement welcome screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'constants/app_theme.dart'; //The soundcloud's theme
 import 'screens/home/home_screen.dart'; //Home screen
+import 'screens/auth/welcome_screen.dart'; //Welcome screen for auth flow
 
 void main() {
   runApp(const SoundCloudApp());
@@ -15,7 +16,8 @@ class SoundCloudApp extends StatelessWidget {
       title: 'SoundCloud',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme, //Take the styling from the app_theme.dart file
-      home: const HomeScreen(),
+      // home: const HomeScreen(), commented out for now, will be replaced by welcome screen
+      home: const WelcomeScreen(), // Temporary welcome screen for auth flow
     );
   }
 }
