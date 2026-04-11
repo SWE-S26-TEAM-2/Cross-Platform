c381141c68678f47cc7400a6e3e6545b20b55340
HanaAhmed30 <hana_ahmed30@yahoo.com>
2026-04-11 22:18:55 +0200

    Sort option to choose the filter

--- lib/screens/library/liked_tracks_screen.dart
+++ lib/screens/library/liked_tracks_screen.dart
@@ -7,9 +7,39 @@ import '../../mock_data/mock_tracks.dart';
 
 enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
  
-class LikedTracksScreen extends StatefulWidget {
-  const LikedTracksScreen({super.key});
+//class LikedTracksScreen extends StatefulWidget {
+ // const LikedTracksScreen({super.key});
+ 
+ // @override
+  //State<LikedTracksScreen> createState() => _LikedTracksScreenState();
+//}
+class _SortOption extends StatelessWidget {
+  final String label;
+  final bool selected;
+  final VoidCallback onTap;
+ 
+  const _SortOption({
+    required this.label,
+    required this.selected,
+    required this.onTap,
+  });
  
   @override
-  State<LikedTracksScreen> createState() => _LikedTracksScreenState();
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      title: Text(
+        label,
+        style: TextStyle(
+          color: selected ? AppColors.primary : AppColors.textPrimary,
+          fontWeight: selected ? FontWeight.bold : FontWeight.normal,
+          fontSize: 15,
+        ),
+      ),
+      trailing: selected
+          ? const Icon(Icons.check, color: AppColors.primary, size: 20)
+          : null,
+      onTap: onTap,
+    );
+  }
 }
