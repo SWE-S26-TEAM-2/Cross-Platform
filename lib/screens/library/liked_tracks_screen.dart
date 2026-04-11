aae3831b9914bdcb18a1bd45ff123456c904781d
HanaAhmed30 <169292385+HanaAhmed30@users.noreply.github.com>
2026-04-11 19:52:08 +0000

    style: auto-format dart code [skip ci]

--- lib/screens/library/liked_tracks_screen.dart
+++ lib/screens/library/liked_tracks_screen.dart
@@ -3,7 +3,8 @@ import '../../constants/app_colors.dart';
 import '../../constants/app_dimensions.dart';
 import '../../constants/app_text_styles.dart';
 import '../../models/track.dart';
-import '../../mock_data/mock_tracks.dart';enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
-  
+import '../../mock_data/mock_tracks.dart';
+
 enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
- 
+
+enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
