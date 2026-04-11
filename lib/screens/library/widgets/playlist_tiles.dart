75b55af9c1119eb7e934ac627a95c26a8560cd1a
HanaAhmed30 <hana_ahmed30@yahoo.com>
2026-04-12 01:22:45 +0200

    Moved tiles to a new folder"widgets" in library

--- lib/screens/library/widgets/playlist_tiles.dart
+++ lib/screens/library/widgets/playlist_tiles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../../constants/app_colors.dart';
-import '../../../constants/app_dimensions.dart';
-import '../../../models/playlist.dart';
+import '../../../../constants/app_colors.dart';
+import '../../../../constants/app_dimensions.dart';
+import '../../../../models/playlist.dart';
 
 class PlaylistTiles extends StatelessWidget {
   const PlaylistTiles({
