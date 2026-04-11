047911c383d0aa28e5b5f0570a53af6aacc54eef
HanaAhmed30 <hana_ahmed30@yahoo.com>
2026-04-11 21:42:02 +0200

    enum of filters to sort the liked tracks

---
+++ lib/screens/library/liked_tracks_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../../constants/app_colors.dart';
+import '../../constants/app_dimensions.dart';
+import '../../constants/app_text_styles.dart';
+import '../../models/track.dart';
+import '../../mock_data/mock_tracks.dart';enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
+  
+enum LikedTracksSortOption { recentlyAdded, firstAdded, trackName, artist }
+ 
