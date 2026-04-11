fd8b634f669622995b27bfaba22e9cf401307176
mohamedshalaby16 <167160800+mohamedshalaby16@users.noreply.github.com>
2026-04-11 15:31:14 +0000

    style: auto-format dart code [skip ci]

--- lib/models/auth_token.dart
+++ lib/models/auth_token.dart
@@ -2,10 +2,7 @@ class AuthTokens {
   final String accessToken;
   final String refreshToken;
 
-  const AuthTokens({
-    required this.accessToken,
-    required this.refreshToken,
-  });
+  const AuthTokens({required this.accessToken, required this.refreshToken});
 
   factory AuthTokens.fromJson(Map<String, dynamic> json) {
     final data = json['data'] as Map<String, dynamic>? ?? json;
